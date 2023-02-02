eea53df194787d27648e5d348546d8be16c5408d
byoso <emailvinz@gmail.com>
2023-02-02 14:27:38 +0100

    minor improvements

--- configs/docker/nginx/run.sh
+++ configs/docker/nginx/run.sh
@@ -2,23 +2,26 @@
 
 set -e
 
-echo "checking for dhparams.pem"
-if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
-    echo "dhparams.pem does not exist, creating it"
-    openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
-fi
+if [ "$SSL" == "1" ]; then
 
-# Avoid replacing this with envsubst
-export host=\$host
-export request_uri=\$request_uri
+    echo "checking for dhparams.pem"
+    if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
+        echo "dhparams.pem does not exist, creating it"
+        openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
+    fi
 
-echo "Checking for fullchain.pem"
-if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
-    echo "No SSL certificate found, enabling HTTP only"
-    envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-else
-    echo "SSL certificate found, enabling HTTPS"
-    envsubst < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
+    # Avoid replacing this with envsubst
+    export host=\$host
+    export request_uri=\$request_uri
+
+    echo "Checking for fullchain.pem"
+    if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
+        echo "No SSL certificate found, enabling HTTP only"
+        envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+    else
+        echo "SSL certificate found, enabling HTTPS"
+        envsubst < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
+    fi
 fi
 
 nginx -g 'daemon off;'
--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -7,8 +7,14 @@ chmod +x migrate.sh
 ./migrate.sh
 cd /web
 
+if [ "$DEBUG" == "1" ]; then
+    echo "DEBUG is 1, starting gunicorn"
+    gunicorn --bind :8000 -w ${WORKERS} project_.wsgi
+else
+    echo "DEBUG is 0, starting uwsgi"
+    uwsgi --socket :8000 --workers ${WORKERS} --master --enable-threads --module project_.wsgi
+fi
 
-uwsgi --socket :8000 --workers ${WORKERS} --master --enable-threads --module project_.wsgi
 
 
 echo "ENTRYPOINT.SH EXECUTED"
