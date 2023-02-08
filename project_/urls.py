40af4679279e00dfa3237a8e81317b0e0c4afb03
byoso <emailvinz@gmail.com>
2023-02-08 02:54:21 +0100

    minor fixes

--- project_/urls.py
+++ project_/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 
 urlpatterns = [
+    # this allows to use {% url 'admin %} in the templates
+    path('radmin', RedirectView.as_view(url=reverse_lazy('admin:index')), name='admin'),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
